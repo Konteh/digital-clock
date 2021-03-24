bcbd3d0d1def3b8c8f34b93aad4dd1c92c53be5e
Konteh <kontehebrima993@gmail.com>
2021-03-24 12:55:26 -0700

    commented on the codes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const clock = document.querySelector('.clock');
 
+// This arrow function will get the hours minutes and seconds
 const tick = () => {
     const now = new Date();
 
@@ -7,7 +8,7 @@ const tick = () => {
     const m = now.getMinutes();
     const s = now.getSeconds();
 
-
+    // This will show the time on the screen
     const html = `
         <span>${h}</span> : 
         <span>${m}</span> : 
@@ -18,4 +19,5 @@ const tick = () => {
     clock.innerHTML = html;
 };
 
+    // The setInterval will change every 1 seconds
 setInterval(tick, 1000 );
